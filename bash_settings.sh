0deef1b4dfe982c3d42be523213e7a1c5762b594
lauren4242 <89677512+lauren4242@users.noreply.github.com>
2026-02-25 11:08:22 -0700

    Update bash_settings.sh prompt configuration
    Comment out the precmd function for ZSH and update the prompt command for BASH.

--- bash_settings.sh
+++ bash_settings.sh
@@ -1,6 +1,9 @@
 #Set the prompt for both ZSH and BASH
 #if [ -n "$ZSH_VERSION"]; then
- # precmd () { __git}
+ # precmd () { __git_ps1 "%n" ":~$ " "|%s"}
+#elif [ -n "$BASH_VERSION"]; then
+# PROMPT_COMMAND='__git_ps1 "\\\$ "'
+
 #Add in alias
 alias ll= 'ls -l'
 alias la='ls -s -a'
